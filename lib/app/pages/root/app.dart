ac695449c2ef3b02ebc162478ac64d53b83e754d
michaelmartins09 <michaelmartins096@gmail.com>
2020-04-29 23:26:44 -0300

    added: flutter framework and root config

---
+++ lib/app/pages/root/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../home/home_page.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'app/pages/root/app.dart';
+
+void main() => runApp(App()); 
