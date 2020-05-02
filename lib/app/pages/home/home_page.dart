5e05533c9b62ad60884b8d458d93f49ac0f74b74
michaelmartins09 <michaelmartins096@gmail.com>
2020-05-01 21:24:17 -0300

    added: player options

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:flutter_visualizers/Visualizers/LineVisualizer.dart';
+import 'package:flutter_visualizers/visualizer.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ui_player_flutter/app/shared/theme/strings.dart';
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -85,8 +89,64 @@ class _HomePageState extends State<HomePage> {
                   ),
                   Text("Brambles", style: TextStyle(
                     color: Colors.grey, fontSize: 16, fontWeight: FontWeight.bold
-                  ))
+                  )),
                 ],
+              ),
+              SizedBox(height: 40),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: 2,
+                      child: LinearProgressIndicator(
+                        value: 0,
+                        backgroundColor: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text("1:04", style: TextStyle(
+                          fontWeight: FontWeight.bold
+                        )),
+                        Text("2:52", style: TextStyle(
+                          color: Colors.grey
+                        )),
+                      ]
+                    ),
+                    SizedBox(height: 40),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        IconButton(icon: Icon(Icons.shuffle, color: Colors.grey), onPressed: (){}),
+                        IconButton(icon: Icon(FontAwesomeIcons.stepBackward, color: Theme.of(context).primaryColor), onPressed: (){}),
+                        Container(
+                          height: 80,
+                          width: 80,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            shape: BoxShape.circle,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Theme.of(context).primaryColor.withOpacity(0.2),
+                                blurRadius: 15,
+                                offset: Offset(0, 15)
+                              )
+                            ]
+                          ),
+                          child: IconButton(
+                            icon: Icon(FontAwesomeIcons.play), 
+                            color: Colors.white,
+                            onPressed: (){}
+                          ),
+                        ),
+                        IconButton(icon: Icon(FontAwesomeIcons.stepForward, color: Theme.of(context).primaryColor), onPressed: (){}),
+                        IconButton(icon: Icon(Icons.repeat, color: Colors.grey), onPressed: (){}),
+                      ]
+                    ),
+                  ],
+                ),
               )
             ],
           ),
